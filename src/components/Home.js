eeab29c6cde60c76cbbe7f303b6f06c3695d0775
mobijalex <mobijalex6@gmail.com>
2023-08-31 17:51:39 +0530

    auto vedio play done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,33 @@
-import { Styled } from "styled-components";
+import { styled } from "styled-components";
+import ImageSlider from "./imageSlider";
+import Viewers from "./Viewers";
 
 const Home = (props) => {
-  return <Container>Home</Container>;
+  return (
+    <Container>
+      <ImageSlider />
+      <Viewers />
+    </Container>
+  );
 };
 
-const Container = Styled.main`
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
 
-
-
+  &:after {
+    background: url("/images/home-background.png") center center fixed no-repeat;
+    background-size: cover;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
 `;
 
 export default Home;
